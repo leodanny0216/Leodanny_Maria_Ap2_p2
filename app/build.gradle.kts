5d41c8f5001c202848751fe5e6c822664b074e46
leodanny0216 <leodannymaria2@gmail.com>
2025-11-05 21:02:28 -0400

    plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation(libs.androidx.room.runtime.android)
     implementation(libs.androidx.media3.common.ktx)
+    implementation(libs.androidx.compose.foundation)
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
